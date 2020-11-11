0877adbc3d62ed483db0a6cd9e94b83ce1171fbd
Rameez Khan <rameezkhan.sa@gmail.com>
2020-11-11 09:27:39 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+http_proxy="$http_proxy"
+
 function info {
     echo "[..] $1"
 }
 
 info "Cloning new-machine repo"
+
+if [[ -n "$http_proxy" ]] ; then
+    info "Looks like you have a proxy set. Setting up git for basic auth for proxy."
+    git config --global http.proxyAuthMethod 'basic'
+fi
+
 git clone https://github.com/rameezk/new-machine
