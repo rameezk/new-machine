634289f20a9b3de0c5dbcc257d8260af0a74a387
Rameez Khan <rameezkhan.sa@gmail.com>
2020-11-11 19:21:27 +0200

    Clone new-machine repo

--- README.md
+++ README.md
@@ -6,9 +6,9 @@
 1. Download [NixOS 20.09](https://channels.nixos.org/nixos-20.09/latest-nixos-plasma5-x86_64-linux.iso)
 2. Boot into installer
 3. If you're behind a proxy, run the steps [specified below](#corporate-proxy)
-4. Run install script with
+4. Run install script with, replacing `<machine-name>` with a machine in `/hosts`
 ``` sh
-curl -s "https://raw.githubusercontent.com/rameezk/new-machine/master/install.sh" > "$HOME/install.sh && chmod +x "$HOME/install.sh" && "$HOME/install.sh"
+curl -s "https://raw.githubusercontent.com/rameezk/new-machine/master/install.sh" > "$HOME/install.sh && chmod +x "$HOME/install.sh" && "$HOME/install.sh <machine>"
 ```
 
 ## Corporate Proxy
--- install.sh
+++ install.sh
@@ -20,14 +20,14 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     curl -s "https://raw.githubusercontent.com/rameezk/new-machine/master/hosts/$machine/partition-and-mount.sh" > "$HOME/$machine-partition-and-mount.sh"
     chmod +x "$HOME/$machine-partition-and-mount.sh"
     "$HOME/$machine-partition-and-mount.sh"
+
+    info "Cloning new-machine repo"
+    if [[ -n "$http_proxy" ]] ; then
+        info "Looks like you have a proxy set. Setting up git with basic auth for proxy."
+        git config --global http.proxyAuthMethod 'basic'
+    fi
+
+    git clone https://github.com/rameezk/new-machine /mnt/etc/nixos
 fi
 
 
-# info "Cloning new-machine repo"
-
-# if [[ -n "$http_proxy" ]] ; then
-#     info "Looks like you have a proxy set. Setting up git for basic auth for proxy."
-#     git config --global http.proxyAuthMethod 'basic'
-# fi
-
-# git clone https://github.com/rameezk/new-machine
